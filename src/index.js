719fbe84e2d017f41320353b7845208b8c4a7303
Artemius Visotski <artemius.visotski@gmail.com>
2018-10-01 01:27:26 +0300

    zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
-}
+  
+  var n = 5;
+  var result = 0;
+  while(number > n){
+    result = result + Math.floor(number / n);
+    n = 5 * n;
+  }
+  return result; 
+};
